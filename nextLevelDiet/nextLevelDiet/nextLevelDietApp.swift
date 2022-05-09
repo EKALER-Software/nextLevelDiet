6d6c1f587c9ea4389be717c796104c4dbc39cc25
Ekrem CIFTCI <62669919+EKALER-Software@users.noreply.github.com>
2022-05-09 14:27:02 -0400

    First commit

---
+++ nextLevelDiet/nextLevelDiet/nextLevelDietApp.swift
@@ -0,0 +1,17 @@
+//
+//  nextLevelDietApp.swift
+//  nextLevelDiet
+//
+//  Created by Ekrem Alp CIFTCI on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct nextLevelDietApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
