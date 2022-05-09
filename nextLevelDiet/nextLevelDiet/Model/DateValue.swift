71ae09f5914b0fd6f89d2d002adb91307270e5b6
Ekrem CIFTCI <62669919+EKALER-Software@users.noreply.github.com>
2022-05-09 17:42:40 -0400

    Added day table

---
+++ nextLevelDiet/nextLevelDiet/Model/DateValue.swift
@@ -0,0 +1,16 @@
+//
+//  DateValue.swift
+//  nextLevelDiet
+//
+//  Created by Ekrem Alp CIFTCI on 5/9/22.
+//
+
+import SwiftUI
+
+//Date value model...
+struct DateValue: Identifiable{
+    var id = UUID().uuidString
+    var day: Int
+    var date: Date
+    
+}
--- nextLevelDiet/nextLevelDiet/View/CustomDatePicker.swift
+++ nextLevelDiet/nextLevelDiet/View/CustomDatePicker.swift
@@ -10,12 +10,22 @@ import SwiftUI
 struct CustomDatePicker: View {
     @Binding var currentDate : Date
     
+    //Month update on arrow button clicks...
+    @State var currentMonth:Int = 0
+    
+    
     var body: some View {
         
-        VStack{
+        VStack (spacing:35){
+            
+            //Days...
+            let days: [String] =
+            ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
+            
+            
             HStack (spacing:20){
                 VStack(alignment: .leading, spacing: 10){
-                    Text("2022")
+                    Text("2021")
                         .font(.caption)
                         .fontWeight(.semibold)
                     Text("September")
@@ -38,7 +48,44 @@ struct CustomDatePicker: View {
                         .font(.title2)
                 }
             }
-            .padding()
+            .padding(.horizontal)
+            //Day view...
+            HStack (spacing:0){
+                
+                ForEach(days,id: \.self){ day in
+                    Text(day)
+                        .font(.callout)
+                        .fontWeight(.semibold)
+                        .frame(maxWidth:.infinity)
+                    
+                }
+            }
+            //Dates...
+            //Lazy grid...
+            let columns = Array(repeating: GridItem(.flexible()), count: 7)
+            LazyVGrid(columns: columns,spacing: 15){
+                ForEach(extractDate()){
+                    value in
+                    Text("\(value.day)")
+                }
+            }
+        }
+    }
+    func extractDate()->[DateValue]{
+        
+        //Getting current month date
+        let calendar = Calendar.current
+        guard let currentMonth = calendar.date(byAdding: .month,value: self.currentMonth, to: Date())
+        else{
+            return[]
+        }
+        return currentMonth.getAllDates().compactMap{date -> DateValue
+            in
+            //Getting day...
+            let day = calendar.component(.day, from: date)
+            
+            return DateValue(day: day, date: date)
+            
         }
     }
 }
@@ -46,5 +93,18 @@ struct CustomDatePicker: View {
 struct CustomDatePicker_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+//Extending date to get current month dates...
+extension Date{
+    func getAllDates()->[Date]{
+        let calendar = Calendar.current
+        let range = calendar.range(of: .day, in: .month, for: self)!
+        //getting date...
+        return range.compactMap{day -> Date in
+            
+            return calendar.date(byAdding: .day, value: day, to: self)!
+        }
     }
 }
